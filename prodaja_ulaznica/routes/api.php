88f6a37936c9a10a2b6d07734b78cad0949c2099
Aleksa Mrakovic <mrakovic.aleksa@gmail.com>
2024-07-14 03:50:12 +0200

    dodavanje venue i artist kontrolera i svih crud operacija za njih

--- prodaja_ulaznica/routes/api.php
+++ prodaja_ulaznica/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\VenueController;
 use App\Http\Controllers\UserTicketController;
 use App\Http\Controllers\API\AuthController;
 
@@ -25,6 +27,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::resource('users.tickets', UserTicketController::class)->only(['index']);
 Route::resource('tickets', TicketController::class)->only(['index']);
+Route::resource('artists', ArtistController::class)->only(['index']);
+Route::resource('venues', VenueController::class)->only(['index']);
 
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
@@ -37,7 +41,17 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/profile', function (Request $request) {
         return auth()->user();
     });
-    Route::resource('tickets', TicketController::class)->only(['update', 'store', 'destroy']);
+    Route::resource('update-tickets', TicketController::class)->only(['update']);
+    Route::resource('buy-tickets', TicketController::class)->only(['store']);
+    Route::resource('delete-tickets', TicketController::class)->only(['destroy']);
+
+    Route::resource('update-artists', ArtistController::class)->only(['update']);
+    Route::resource('buy-artists', ArtistController::class)->only(['store']);
+    Route::resource('delete-artists', ArtistController::class)->only(['destroy']);
+
+    Route::resource('update-venues', VenueController::class)->only(['update']);
+    Route::resource('buy-venues', VenueController::class)->only(['store']);
+    Route::resource('delete-venues', VenueController::class)->only(['destroy']);
     // API route for logout user
     Route::post('/logout', [AuthController::class, 'logout']);
 });
